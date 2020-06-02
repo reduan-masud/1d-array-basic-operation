7ab509c0c749c71ae7ba3c0e79a9a82893071762
reduan-masud <redouanmasud@gmail.com>
2020-06-02 22:44:55 +0600

    up

--- Problem4.cpp
+++ Problem4.cpp
@@ -2,9 +2,11 @@
 
 int main()
 {
-
+    freopen("input.txt", "r", stdin);
     int arr[100] ={10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int n = 10;
+    //int n = 10;
+    int n;
+    scanf("%d", &n);
     int i;
     for(i = 0; i<n; i++)
     {
